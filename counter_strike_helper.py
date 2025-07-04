e8f6996285a92ba2a9a564487ff6625476eeee35
scottwang <hanqing.a350@gmail.com>
2025-07-04 11:00:33 -0700

    Sending

--- counter_strike_helper.py
+++ counter_strike_helper.py
@@ -144,12 +144,12 @@ def icmpflood(target_url, cycle):
     all_threads = []
     for target in targets:
         for x in range(0, int(cycle)):
-                t1 = threading.Thread(target=attack_icmp_helper, args=target)
-                t1.start()
-                all_threads.append(t1)
+            t1 = threading.Thread(target=attack_icmp_helper, args=target)
+            t1.start()
+            all_threads.append(t1)
 
-                # Adjusting this sleep time will affect requests per second
-                time.sleep(0.01)
+            # Adjusting this sleep time will affect requests per second
+            time.sleep(0.01)
 
         for current_thread in all_threads:
             current_thread.join()  # Make the main thread wait for the children threads
@@ -188,7 +188,7 @@ def attack_xmas_helper(target, targetPort):
 
 
 def xmasflood(target_url, targetPort, cycle):
-    all_threads= []
+    all_threads = []
     targets = resolve_to_ipv4(target_url)
     for target in targets:
         for x in range(0, int(cycle)):
@@ -201,7 +201,6 @@ def xmasflood(target_url, targetPort, cycle):
 
         for current_thread in all_threads:
             current_thread.join()  # Make the main thread wait for the children threads
-
 
 
 # # Parse inputs
@@ -229,7 +228,7 @@ def attack_http_flood(target, target_port, num_of_requests):
     # Spawn a thread per request
     all_threads = []
     for i in range(num_requests):
-        t1 = threading.Thread(target=attack_http_flood_helper, args=(ip,port,host))
+        t1 = threading.Thread(target=attack_http_flood_helper, args=(ip, port, host))
         t1.start()
         all_threads.append(t1)
 
@@ -260,7 +259,7 @@ def generate_url_path():
 
 
 # Perform the request
-def attack_http_flood_helper(ip,port,host):
+def attack_http_flood_helper(ip, port, host):
     print_status()
     url_path = generate_url_path()
 
